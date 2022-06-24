fd6ef250e4ce3103aaf90c4ab43eb8ae94a0c82e
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2022-06-23 22:50:50 -0600

    preddcion final

--- proyecto-final/src/com/heatonresearch/book/introneuralnet/ch9/predict/ActualData.java
+++ proyecto-final/src/com/heatonresearch/book/introneuralnet/ch9/predict/ActualData.java
@@ -1,81 +1,124 @@
-
+/**
+ * Introduction to Neural Networks with Java, 2nd Edition
+ * Copyright 2008 by Heaton Research, Inc. 
+ * http://www.heatonresearch.com/books/java-neural-2/
+ * 
+ * ISBN13: 978-1-60439-008-7  	 
+ * ISBN:   1-60439-008-5
+ *   
+ * This class is released under the:
+ * GNU Lesser General Public License (LGPL)
+ * http://www.gnu.org/copyleft/lesser.html
+ */
 package com.heatonresearch.book.introneuralnet.ch9.predict;
 
-import java.io.BufferedReader;
+//Mio
+import java.io. * ;
+import java.util.Scanner;
+
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.IOException;//signals an exception of some kind has occurred
+import java.io.BufferedReader;//Reads text from a character-input stream
+//Mio
+
+/**
+ * Chapter 9: Predictive Neural Networks
+ * 
+ * ActualData: Holds values from the sine wave.
+ * 
+ * @author Jeff Heaton
+ * @version 2.1
+ */
+
 
 public class ActualData {
+	
+	
+	public static double datos_clima[] = new double[10000];
+	
 	public static double sinDEG(final double deg) {
-		final double rad = deg * (Math.PI / 180);
+		
+	    final double rad = deg * (Math.PI / 180);
 		final double result = Math.sin(rad);
 		return ((int) (result * 100000.0)) / 100000.0;
+		
 	}
-
+	
 	private final double actual[];
 	private final int inputSize;
-        private final double datos_clima[] ;
-        //Double[] datos_clima = new Double[1000];
-        
+
 	private final int outputSize;
-        private int segundo_dataset = 0;
-	public ActualData(final int size, final int inputSize, final int outputSize, final double nuevo_valor, final int contador_5, final int size_original, final int cantidad_datos_salida) {
-                //Codigo anterior NO MOVER
-                this.actual = new double[size];
+	
+	public static int segundo_dataset;
+	public static double valor_minimo_predicho;
+	public static double valor_medio_predicho;
+	public static double valor_maximo_predicho;
+	
+
+	public ActualData(final int size, final int inputSize, final int outputSize, 
+			final double nuevo_valor, final int contador_5, final int size_original, final int cantidad_datos_salida){
+		
+		
+		String sample = ",";
+        String mystring;
+        try
+        {
+        	segundo_dataset++;
+        	
+        	BufferedReader temperatura;
+        	
+        	if (segundo_dataset == 1) {
+        		temperatura = new BufferedReader(new FileReader("temp_min.csv"));
+        	}else {
+            	temperatura = new BufferedReader(new FileReader("temp_max.csv"));  
+        	}
+        	
+            int i = 0;
+            
+            while ((mystring = temperatura.readLine()) != null)  //Reads a line of text
+            {
+                String[] clima = mystring.split(sample);//utilized to split the string
+                    datos_clima[i] = Double.parseDouble(clima[0])/100;
+                i++;	
+            }
+            
+            if (size > size_original) {
+                datos_clima[size-1] = nuevo_valor;
+            }
+        }
+        catch (IOException e)//catches exception in the try block
+        {
+            e.printStackTrace();//Prints this throwable and its backtrace
+        }
+		
+		
+		this.actual = new double[size];
 		this.inputSize = inputSize;
 		this.outputSize = outputSize;
-                String sample = ",";
-                String mystring;
-                
-                try
-                {
-                    segundo_dataset++;
-                    
-                    BufferedReader temperatura; //Leer archivo
-                    //Los archivos deben ir en la raiz del proyecto
-                    if(segundo_dataset == 1){ //Tempeartura minima
-                        temperatura = new BufferedReader(new FileReader("temp_min.csv"));
-                    }
-                    else{ //Temperatura maxima
-                        temperatura = new BufferedReader(new FileReader("temp_max.csv"));
-                    }
-                    
-                    int i = 0;
-                    
-                    while ((mystring = temperatura.readLine()) != null){ //Leemos linea por linea el archivo
-                        String[] clima = mystring.split(sample); //Separamos por comas
-                        datos_clima[i] = Double.parseDouble(clima[0])/100;
-                        i++;
-                    }
-                    
-                    if(size > size_original){
-                        datos_clima[size-1] = nuevo_valor;
-                    }
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-            
-                for(int i = 0; i < size; i++){
-                    this.actual[i] = datos_clima[i];
-                    
-                    if(i == size_original +1){
-                        double valor_minimo_predicho = ((int) (actual[size_original]*10000.0))/100.0;
-                        double valor_maximo_predicho = ((int) (actual[size_original]*10000.0))/100.0;
-                        
-                        System.out.println("Valor máximo: " + valor_maximo_predicho);
-                        System.out.println("Valor minimo: " + valor_minimo_predicho);
-                    }
-                }
-                
-                //Salimos del actual data
-                if (size > size_original + cantidad_datos_salida-1){
-                    System.out.println("Salimos del actualData");
-                    System.exit(0);
-                }
-                
+
+
+		for (int i = 0; i < size; i++) {
+			this.actual[i] = datos_clima[i];
+			
+			if (i == size_original +1) {
+
+					valor_minimo_predicho = ((int) (actual[size_original]     * 10000.0)) / 100.0;
+					valor_maximo_predicho = ((int) (actual[size_original + 1] * 10000.0)) / 100.0;
+					
+					System.out.println("Valor minimo predicho: - " + valor_minimo_predicho);
+					System.out.println("Valor maximo predicho: - " + valor_maximo_predicho);					
+			}
+		}
+		
+		
+		//Salir cuando se alcance los que se quieren calcular
+		if (size > size_original + cantidad_datos_salida-1) {	
+			System.out.println("Salir ActualData");
+			System.exit(0);
+		}
+		
 	}
+	
 
 	public void getInputData(final int offset, final double target[]) {
 		for (int i = 0; i < this.inputSize; i++) {
@@ -88,6 +131,4 @@ public class ActualData {
 			target[i] = this.actual[offset + this.inputSize + i];
 		}
 	}
-	
-
 }
--- proyecto-final/src/com/heatonresearch/book/introneuralnet/ch9/predict/SinWave.java
+++ proyecto-final/src/com/heatonresearch/book/introneuralnet/ch9/predict/SinWave.java
@@ -1,3 +1,15 @@
+/**
+ * Introduction to Neural Networks with Java, 2nd Edition
+ * Copyright 2008 by Heaton Research, Inc. 
+ * http://www.heatonresearch.com/books/java-neural-2/
+ * 
+ * ISBN13: 978-1-60439-008-7  	 
+ * ISBN:   1-60439-008-5
+ *   
+ * This class is released under the:
+ * GNU Lesser General Public License (LGPL)
+ * http://www.gnu.org/copyleft/lesser.html
+ */
 package com.heatonresearch.book.introneuralnet.ch9.predict;
 
 import java.text.NumberFormat;
@@ -11,32 +23,44 @@ import com.heatonresearch.book.introneuralnet.neural.feedforward.train.anneal.Ne
 import com.heatonresearch.book.introneuralnet.neural.feedforward.train.backpropagation.Backpropagation;
 import com.heatonresearch.book.introneuralnet.neural.util.ErrorCalculation;
 
+/**
+ * Chapter 9: Predictive Neural Networks
+ * 
+ * SinWave: Use a neural network to predict the sine wave.
+ * 
+ * @author Jeff Heaton
+ * @version 2.1
+ */
 public class SinWave {
-	//Variables de configuración del programa
-        public final static int ACTUAL_SIZE = 525; //Cantidad de datos
-        public final static int TRAINING_SIZE = 250; //Cantidad de entrenamiento
-        public final static int INPUT_SIZE = 5; //Cantidad de neuronas de entrada
-        public final static int OUTPUT_SIZE = 1; //Cantidad de neuronas de salida
-        public final static int NEURONS_HIDDEN_1 = 7; //Cantidad de neuronas ocultas
-        public final static int NEURONS_HIDDEN_2 = 0;
-        public final static boolean USE_BACKPROP = true; //Entrenar con backpropagation
-        public static int cantidad_datos_salida = 2; //Cantidad de predicciones que necesitamos
-        public static double valor_nuevo = 0; //el valor a precedir se guarda acá
-        public static int contador_5_mandar = 0; 
-        public static int ACTUAL_SIZE_OG = ACTUAL_SIZE; //Valor actual original
-        public static int solo_calcular_1_vez = 0;
-        int segundo_dataset = 0;
+	public static int ACTUAL_SIZE = 525; //Tama�o del Dataset
+	public static int cantidad_datos_salida = 2; //Cantidad de predicciones que debe de hacer
+	public static double valor_nuevo = 0;
+	public static int contador_5_mandar = 0;
+	public static int ACTUAL_SIZE_OG = ACTUAL_SIZE;
+	public static int solo_calcular_1_vez = 0;
+	public final static int TRAINING_SIZE = 250;
+	public final static int INPUT_SIZE = 5;
+	public final static int OUTPUT_SIZE = 1;
+	public final static int NEURONS_HIDDEN_1 = 7;
+	public final static int NEURONS_HIDDEN_2 = 0;
+	public final static boolean USE_BACKPROP = true;
 
 	public static void main(final String args[]) {
 		final SinWave wave = new SinWave();
 		wave.run();
 	}
 
-	private ActualData actual;
+	private ActualData actual;	
 	private double input[][];
-
 	private double ideal[][];
 
+	public static int segundo_dataset;
+	
+	public static double calcular_5[] = new double[1000];
+	private int contador_5 = 0;
+	private int contador_500 = 0;
+	
+	
 	private FeedforwardNetwork network;
 
 	public void createNetwork() {
@@ -60,13 +84,15 @@ public class SinWave {
 		final double input[] = new double[SinWave.INPUT_SIZE];
 		final double output[] = new double[SinWave.OUTPUT_SIZE];
 
+		//Comienza en el 5 por el WinWave.Input_size
 		for (int i = SinWave.INPUT_SIZE; i < SinWave.ACTUAL_SIZE; i++) {
+
+			
 			this.actual.getInputData(i - SinWave.INPUT_SIZE, input);
 			this.actual.getOutputData(i - SinWave.INPUT_SIZE, output);
 
 			final StringBuilder str = new StringBuilder();
-			str.append(i);
-			str.append(":Actual=");
+
 			for (int j = 0; j < output.length; j++) {
 				if (j > 0) {
 					str.append(',');
@@ -76,21 +102,47 @@ public class SinWave {
 
 			final double predict[] = this.network.computeOutputs(input);
 
-			str.append(":Predicted=");
+			str.append(",");
 			for (int j = 0; j < output.length; j++) {
 				if (j > 0) {
 					str.append(',');
 				}
 				str.append(predict[j]);
+				
+				if (contador_500 == ACTUAL_SIZE - INPUT_SIZE - 1) {//=================================================================================
+
+					calcular_5[contador_5] = predict[0];
+					
+					SinWave.ACTUAL_SIZE = SinWave.ACTUAL_SIZE + 1;
+					SinWave.valor_nuevo = calcular_5[contador_5];
+
+					//Dato calculado
+					contador_5++;
+					SinWave.contador_5_mandar = contador_5;
+					
+					
+					final SinWave wave = new SinWave();
+					wave.run();
+					
+				}
+				
+				contador_500++;
+				
 			}
 
-			str.append(":Difference=");
+//			str.append(":Difference=");
+			str.append(",");
 
 			final ErrorCalculation error = new ErrorCalculation();
 			error.updateError(predict, output);
 			str.append(percentFormat.format(error.calculateRMS()));
 
-			System.out.println(str.toString());
+
+			if (i > ACTUAL_SIZE_OG - 5) { //Cambiar a -3 para que solo muestre los �ltimos
+
+//				System.out.println(str.toString()); MOSTRAR
+
+			}
 		}
 	}
 
@@ -110,13 +162,14 @@ public class SinWave {
 				}
 				str.append(this.ideal[i][j]);
 			}
-			System.out.println(str.toString());
+			
 		}
 
 	}
 
 	private void generateActual() {
-		this.actual = new ActualData(SinWave.ACTUAL_SIZE, SinWave.INPUT_SIZE, SinWave.OUTPUT_SIZE, SinWave.valor_nuevo, SinWave.contador_5_mandar, SinWave.ACTUAL_SIZE_OG, SinWave.cantidad_datos_salida);
+		this.actual = new ActualData(SinWave.ACTUAL_SIZE, SinWave.INPUT_SIZE,
+				SinWave.OUTPUT_SIZE, SinWave.valor_nuevo, SinWave.contador_5_mandar, SinWave.ACTUAL_SIZE_OG, SinWave.cantidad_datos_salida);
 	}
 
 	private void generateTrainingSets() {
@@ -130,40 +183,47 @@ public class SinWave {
 	}
 
 	public void run() {
+		
 		generateActual();
 		createNetwork();
 		generateTrainingSets();
 
-                trainNetworkBackprop(); //Entrenamiento
+		//Entrenar
+		trainNetworkBackprop();
 		
 		display();
-
+		
 	}
 
+
 	private void trainNetworkBackprop() {
-            if(solo_calcular_1_vez == 0){
-                final Train train = new Backpropagation(this.network, this.input,
+		
+		//Solo calcular 1 vez
+		if (solo_calcular_1_vez == 0) {
+		
+		final Train train = new Backpropagation(this.network, this.input,
 				this.ideal, 0.001, 0.1);
 
 		int epoch = 1;
-                
 
-                if(segundo_dataset == 0){
-                    System.out.println("Entrenar la primera red para predecir el valor minimo");
-                }else{
-                    System.out.println("Entrenar la segunda red para predecir el valor maximo");
-                }
-		do {
-			train.iteration();
-                        if(epoch % 100 == 0){
-                            System.out.println("Iteración #" + epoch + " Error:"
-                                + train.getError());
-			}
-                        epoch++;
-		} while ((epoch < 50000) && (train.getError() > 0.00001));
-                
-                segundo_dataset++;
-                System.out.println("---------------------------------------------------------------");
-            }
+			//C�digo original
+		if (segundo_dataset == 0) {
+			System.out.println("Entrenar la primera red para predecir el valor minimo");
+		}else {
+			System.out.println("Entrenar la segunda red para predecir el valor maximo");
+		}
+			do {
+				train.iteration();
+				if (epoch % 100 == 0) {
+					System.out.println("Iteration #" + epoch + " Error:"
+					+ train.getError());				
+				}
+				epoch++;
+			} while ((epoch <= 5000) && (train.getError() > 0.001));
+			//C�digo original
+			
+			segundo_dataset++;
+			System.out.println("=====================================================");
+		}
 	}
 }
